bff7efd9f89e9225412966c961c69664258b912f
Richard Slater <richard@slaterade.com>
2021-10-07 19:16:19 -0700

    oops

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    const decodedToken = jwt.verify(token, process.env.CRYPTO_KEY);
     req.userData = { email: decodedToken.email, userId: decodedToken.userId };
     next();
   } catch (error) {
